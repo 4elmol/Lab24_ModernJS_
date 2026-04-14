cd1498e917aa77890ff2650001b647568a6a5a35
4elmol <ne4eLoDoIMoLoVeK@yandex.ru>
2026-04-14 11:17:16 +0300

    kruto

---
+++ js/main.js
@@ -0,0 +1,187 @@
+// console.log("\n=== Деструктуризация массивов ===");
+
+// const colors = ["red", "green", "blue"];
+
+// const color1 = colors[0];
+// const color2 = colors[1];
+// console.log(color1, color2);
+
+// const [firstColor, secondColor, thirdColor] = colors;
+// console.log(firstColor, secondColor, thirdColor);
+
+// const [primary, , tertiary] = colors;
+// console.log(primary, tertiary);
+
+// const [c1, c2, c3, c4 = "yellow"] = colors;
+
+// console.log("Деструктуризация объектов");
+
+// const user = {
+//   name: "Алиса",
+//   age: 25,
+//   city: "Москва",
+// };
+
+// const userName = user.name;
+// const userAge = user.age;
+// console.log(userName, userAge);
+
+// const { name, age, city } = user;
+// console.log(name, age, city);
+
+// const { name: fullName, age: years } = user;
+// console.log(fullName, years);
+
+// const { name: personName, country = "Россия" } = user;
+// console.log(personName, country);
+
+// console.log("Деструктуризация в параметрах");
+
+// function printUserOld(user) {
+//     console.log(`Имя: ${user.name}`);
+//     console.log(`Возраст: ${user.age}`);
+//     console.log(`Город: ${user.city}`);
+// }
+
+// function printUser({ name, age, city }) {
+//   console.log(`Имя: ${name}`);
+//   console.log(`Возраст: ${age}`);
+//   console.log(`Город: ${city}`);
+// }
+
+// printUserId(user);
+// console.log();
+// printUser(user);
+
+// console.log("Spread для массивов");
+// const arr1 = [1, 2, 3];
+// const arr2 = [4, 5, 6];
+
+// const combined = [...arr1, ...arr2];
+// console.log("Объединённый массив:", combined);
+
+// const copy = [...arr1];
+// console.log("Копия массива:", copy);
+
+// const extended = [0, ...arr1, 7, 8];
+// console.log("Расширенный массив:", extended);
+
+// console.log("Spread для объктов");
+// const person = {
+//   name: "Иван",
+//   age: 30,
+// };
+
+// const address = {
+//   city: "Санкт-Петербург",
+//   street: "Невский проспект",
+// };
+
+// const fullInfo = { ...person, ...address };
+// console.log("Полная информация:", fullInfo);
+
+// const personCopy = { ...person };
+// console.log("Копия объекта:", fullInfo);
+
+// const updated = { ...person, age: 31, occupation: "Developer" };
+// console.log("Обновлённый объект:", updated);
+
+// console.log("Rest оператор");
+
+// function sum(...numbers) {
+//     return numbers.reduce((total, num) => total + num, 0);
+// }
+
+// console.log("Сумма 1,2,3:", sum(1, 2, 3));
+// console.log("Сумма 1,2,3,4,5:", sum(1, 2, 3, 4, 5));
+
+// const numbers = [10, 20, 30, 40, 50];
+// const [first, second, ...rest] = numbers;
+// console.log("Первое число", first);
+// console.log("Второе число", second);
+// console.log("Остальные числа", rest);
+
+export function greet(name) {
+  return `Привет, ${name}!`;
+}
+
+export function add(a, b) {
+  return a + b;
+}
+
+export function multiply(a, b) {
+  return a * b;
+}
+
+export const PI = 3.14159;
+
+export default function sayHello() {
+  return "Привет из модуля utils!";
+}
+
+console.log("Промисы");
+const simplePromise = new Promise((resolve, reject) => {
+  const success = true;
+  if (success) {
+    resolve("Операция выполнена успешно!");
+  } else {
+    reject("Произошла ошибка!");
+  }
+});
+
+simplePromise
+  .then((result) => console.log("Результат:", result))
+  .catch((error) => console.log("Ошибка:", error));
+
+function delay(ms) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Прошло ${ms} миллисекунд`);
+    }, ms);
+  });
+}
+delay(1000).then((message) => console.log(message));
+
+function fetchUserData(userId) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (userId > 0) {
+        resolve({
+          id: userId,
+          name: "Иван Иванов",
+          email: "ivan@example.com",
+        });
+      } else {
+        reject("Неверный ID пользователя");
+      }
+    }, 1500);
+  });
+}
+
+fetchUserData(1)
+  .then((user) => console.log("Пользователь:", user))
+  .catch((error) => console.log("Ошибка:", error));
+
+function step1() {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve("Шаг 1 завершён"), 500);
+  });
+}
+
+function step2() {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(`${previousResult} -> Шаг 2 завершён`), 500);
+  });
+}
+
+function step3() {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(`${previousResult} -> Шаг 3 завершён`), 500);
+  });
+}
+
+step1()
+  .then((result1) => step2(result1))
+  .then((result2) => step3(result2))
+  .then((finalResult) => console.log("Финальный результат:", finalResult))
+  .catch((error) => console.log("Ошибка в цепочке:", error));
---
+++ js/utils.js
@@ -0,0 +1,12 @@
+// import { greet, add, PI} from "/utils.js";
+// console.log("Модули");
+// console.log(greet("Алексей"));
+// console.log("5 + 3 =", add(5, 3));
+// console.log("Значение PI", PI);
+
+// import { multiply as умножить } from "./utils.js";
+// console.log("4 * 7 =", умножить(4, 7));
+
+// import * as Utils from "./utils.js";
+// console.log(Utils.greet("Мария"));
+// console.log("Умножение:", Utils.multiply(3, 9));
